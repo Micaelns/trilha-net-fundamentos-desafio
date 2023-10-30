dec95d980e8d1b2f5159ff3aa6be13e57ad8cf59
Micael Souza <micael.ns@hotmail.com>
2023-10-30 19:04:26 -0300

    desafio concluído

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -52,8 +52,10 @@ while (exibirMenu)
             break;
     }
 
-    Console.WriteLine("Pressione uma tecla para continuar");
-    Console.ReadLine();
+    if (exibirMenu) {
+        Console.WriteLine("Pressione uma tecla para continuar");
+        Console.ReadLine();
+    }
 }
 
 Console.WriteLine("O programa se encerrou");
